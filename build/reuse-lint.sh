badb9d354bef994a223acbc33a8287176d1237d1
pudelkoM <pudelkoM@users.noreply.github.com>
2020-10-29 15:45:06 -0700

    Add license and enforce with REUSE

---
+++ build/reuse-lint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Copyright 2020-present Open Networking Foundation
+# SPDX-License-Identifier: LicenseRef-ONF-Member-1.0
+
+set -ex
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+DBUF_ROOT_DIR=$(dirname "$DIR")
+
+docker run --rm -v "${DBUF_ROOT_DIR}":/dbuf -w /dbuf omecproject/reuse-verify:latest reuse lint
