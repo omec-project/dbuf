806473aeeb30d929f215d5bf8b7073ac2cf397e8
pudelkoM <pudelkoM@users.noreply.github.com>
2021-02-01 15:24:33 -0800

    Validate log level flag values

--- cmd/dbuf/main.go
+++ cmd/dbuf/main.go
@@ -5,22 +5,59 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/omec-project/dbuf"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
 	logLevel  = flag.String("log_level", "debug", "Verbosity of the logs")
-	logFormat = flag.String("log_format", "text", "Format of the logs")
+	logFormat = &FormatFlagValue{&log.TextFormatter{}}
 )
+
+func init() {
+	flag.Var(logFormat, "log_format", "Format of the logs")
+}
+
+type FormatFlagValue struct {
+	formatter log.Formatter
+}
+
+func (v *FormatFlagValue) String() string {
+	if _, ok := v.formatter.(*log.TextFormatter); ok {
+		return "text"
+	}
+	if _, ok := v.formatter.(*log.JSONFormatter); ok {
+		return "json"
+	}
+
+	return "unknown"
+}
+
+func (v *FormatFlagValue) Set(s string) error {
+	switch s {
+	case "text":
+		v.formatter = &log.TextFormatter{}
+	case "json":
+		v.formatter = &log.JSONFormatter{}
+	default:
+		return fmt.Errorf("allowed values are: text, json")
+	}
+
+	return nil
+}
+
+func (v *FormatFlagValue) GetFormatter() log.Formatter {
+	return v.formatter
+}
 
 func main() {
 	flag.Parse()
 	log.SetReportCaller(true)
-	log.SetFormatter(&log.JSONFormatter{})
+	log.SetFormatter(logFormat.GetFormatter())
 	level, err := log.ParseLevel(*logLevel)
 	if err != nil {
-		log.Fatal("Could not parse log level:", err)
+		log.Fatalf("Could not parse log level: %v", err)
 	}
 	log.SetLevel(level)
 
